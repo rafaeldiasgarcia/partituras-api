59909b9942a7cd5f3a4806429f68963e9f5d9c76
rafaeldiasgarcia <rafdgar@gmail.com>
2026-03-01 01:08:22 -0300

    data exemplo

---
+++ data/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO partitura (titulo, autor, arranjador, bpm, tonalidade, avaliacao) VALUES
+                                                                                  ('Asturias (Leyenda)', 'Isaac Albéniz', 'Andrés Segovia', 120, 'Em', 5),
+                                                                                  ('Recuerdos de la Alhambra', 'Francisco Tárrega', 'Francisco Tárrega', 80, 'Am', 5),
+                                                                                  ('Romance Anônimo', 'Tradicional', 'Narciso Yepes', 88, 'Em', 4),
+                                                                                  ('Bachianinha No. 1', 'Paulinho Nogueira', 'Paulinho Nogueira', 140, 'Am', 5),
+                                                                                  ('Sons de Carrilhões', 'João Pernambuco', 'João Pernambuco', 92, 'D', 5),
+                                                                                  ('Bourrée em Mi Menor', 'J.S. Bach', 'Julian Bream', 100, 'Em', 5),
+                                                                                  ('Capricho Árabe', 'Francisco Tárrega', 'Francisco Tárrega', 72, 'Dm', 5),
+                                                                                  ('Choro N. 1', 'Heitor Villa-Lobos', 'Villa-Lobos', 96, 'Em', 5);
